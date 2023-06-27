841ca48deedc646cc2258ed46a35a6264e36b1ed
Eli <104906733+Bonnet2B1@users.noreply.github.com>
2023-06-28 01:39:28 +0200

    save

--- SRCS/animation.c
+++ SRCS/animation.c
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   render_and_input.c                                 :+:      :+:    :+:   */
+/*   animation.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edelarbr <edelarbr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/09 01:12:08 by edelarbr          #+#    #+#             */
-/*   Updated: 2023/06/28 01:28:50 by edelarbr         ###   ########.fr       */
+/*   Created: 2023/06/28 01:29:27 by edelarbr          #+#    #+#             */
+/*   Updated: 2023/06/28 01:31:13 by edelarbr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	render_and_inputs(t_map *m)
+void	render_animation(t_map *m)
 {
-	m->mlx = mlx_init(m->width * 64, (m->height + 1) * 64, "so_long", true);
-	render_map(m, NULL);
-	mlx_key_hook(m->mlx, &key, m);
-	mlx_loop_hook(s_map->mlx, &portal_anim, s_map);
-	mlx_loop(m->mlx);
+	
+}
+
+void	portal_anim(void *param)
+{
+	t_map	*m;
+
+	m = param;
+	if (m->collectible == 0)
+		render_animation(m);
 }
